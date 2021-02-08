826133facf28631f46bd3a12d959b17bfda7065c
DUAL <dual1tyx@hotmail.co.uk>
2021-02-08 16:15:14 +0000

    -final

--- src/PusherErrorException.php
+++ src/PusherErrorException.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace ApiClients\Client\Pusher;
 
 use Exception;
 
-final class PusherErrorException extends Exception
+class PusherErrorException extends Exception
 {
 }
